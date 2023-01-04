1a3449a84216399fa9f1bf4862086ab90ed60331
Michael Kriese <michael.kriese@visualon.de>
2023-01-04 16:00:59 +0100

    build: add more changelog types

--- tools/changelog.mjs
+++ tools/changelog.mjs
@@ -1,10 +1,63 @@
 import conventionalChangelogCore from 'conventional-changelog-core';
 import conventionalChangelogPreset from 'conventional-changelog-conventionalcommits';
-import fs from "node:fs"
+import fs from 'node:fs';
 
-const config = conventionalChangelogPreset();
+const config = conventionalChangelogPreset({
+  types: [
+    {
+      type: 'feat',
+      section: 'Features',
+    },
+    {
+      type: 'feature',
+      section: 'Features',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes',
+    },
+    {
+      type: 'perf',
+      section: 'Performance Improvements',
+    },
+    {
+      type: 'revert',
+      section: 'Reverts',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation',
+    },
+    {
+      type: 'style',
+      section: 'Styles',
+    },
+    {
+      type: 'chore',
+      section: 'Miscellaneous Chores',
+    },
+    {
+      type: 'refactor',
+      section: 'Code Refactoring',
+    },
+    {
+      type: 'test',
+      section: 'Tests',
+    },
+    {
+      type: 'build',
+      section: 'Build System',
+    },
+    {
+      type: 'ci',
+      section: 'Continuous Integration',
+    },
+  ],
+});
 
-const file = process.argv[3] ? fs.createWriteStream(process.argv[3]): process.stdout;
+const file = process.argv[3]
+  ? fs.createWriteStream(process.argv[3])
+  : process.stdout;
 
 conventionalChangelogCore(
   {
